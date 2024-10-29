28364c4dcdeda0e4bc9d70fc3c256b4ff72d1e0b
Ruhollah Jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2024-10-29 23:59:17 +0330

    TwoSum

--- Solution/ConsoleApp/Program.cs
+++ Solution/ConsoleApp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numDict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (numDict.ContainsKey(complement))
+            {
+                return new int[] { numDict[complement], i };
+            }
+            numDict[nums[i]] = i;
+        }
+
+        throw new ArgumentException("No two sum solution");
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = TwoSum(nums, target);
+        Console.WriteLine($"[{result[0]}, {result[1]}]"); // Output: [0, 1]
+    }
+}
