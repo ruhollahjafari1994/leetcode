e689aaa2d103c870d1c3d01547bd26bbdfb05f86
Ruhollah Jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2024-10-31 23:51:49 +0330

    problems/median-of-two-sorted-arrays

--- Solution/ConsoleApp/Program.cs
+++ Solution/ConsoleApp/Program.cs
@@ -4,38 +4,83 @@ class Program
 {
     public static void Main(string[] args)
     {
+        int[] nums1 = { 1, 3 };
+        int[] nums2 = { 2 };
         Solution solution = new Solution();
+        double result = solution.FindMedianSortedArrays(nums1, nums2);
+        Console.WriteLine(result);
 
-        Console.WriteLine(solution.LengthOfLongestSubstring("abcabcbb")); // Output: 3
-        Console.WriteLine(solution.LengthOfLongestSubstring("bbbbb"));    // Output: 1
-        Console.WriteLine(solution.LengthOfLongestSubstring("pwwkew"));   // Output: 3
     }
 
-}
-public class Solution
+    public class Solution
 {
-    public int LengthOfLongestSubstring(string s)
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
     {
-        if (string.IsNullOrEmpty(s))
-        {
-            return 0;
-        }
+        // اطمینان از اینکه nums1 کوتاه‌تر از nums2 است
+        if (nums1.Length > nums2.Length)
+            return FindMedianSortedArrays(nums2, nums1);
 
-        int maxLength = 0;
-        int start = 0;
-        var charIndexMap = new Dictionary<char, int>();
+        int m = nums1.Length;
+        int n = nums2.Length;
+        int halfLen = (m + n + 1) / 2;
+        int minIndex = 0, maxIndex = m;
 
-        for (int end = 0; end < s.Length; end++)
+        while (minIndex <= maxIndex)
         {
-            if (charIndexMap.ContainsKey(s[end]))
+            int i = (minIndex + maxIndex) / 2;
+            int j = halfLen - i;
+
+            if (i < m && nums2[j - 1] > nums1[i])
             {
-                start = Math.Max(start, charIndexMap[s[end]] + 1);
+                minIndex = i + 1;  // افزایش i برای جستجو بیشتر
             }
+            else if (i > 0 && nums1[i - 1] > nums2[j])
+            {
+                maxIndex = i - 1;  // کاهش i برای جستجو کمتر
+            }
+            else
+            {
+                // حالت مطلوب را پیدا کردیم
+                int maxOfLeft;
+                if (i == 0)
+                {
+                    maxOfLeft = nums2[j - 1];
+                }
+                else if (j == 0)
+                {
+                    maxOfLeft = nums1[i - 1];
+                }
+                else
+                {
+                    maxOfLeft = Math.Max(nums1[i - 1], nums2[j - 1]);
+                }
 
-            charIndexMap[s[end]] = end;
-            maxLength = Math.Max(maxLength, end - start + 1);
+                if ((m + n) % 2 == 1)
+                {
+                    return maxOfLeft; // اگر تعداد کل عناصر فرد باشد، میانه maxOfLeft خواهد بود
+                }
+
+                int minOfRight;
+                if (i == m)
+                {
+                    minOfRight = nums2[j];
+                }
+                else if (j == n)
+                {
+                    minOfRight = nums1[i];
+                }
+                else
+                {
+                    minOfRight = Math.Min(nums1[i], nums2[j]);
+                }
+
+                return (maxOfLeft + minOfRight) / 2.0; // اگر تعداد کل عناصر زوج باشد، میانه میانگین maxOfLeft و minOfRight است
+            }
         }
 
-        return maxLength;
+        throw new ArgumentException("Input arrays are not valid");
     }
 }
+
+}
+ 
