aee3d9cc55d57a9a1a976ae32f36f81eadd1a596
Ruhollah JAfari <jruhollah@gmail.com>
2024-11-05 23:46:28 +0330

    Step-by-Step Instructions

--- TestProject/PropTradingSystem/Models/Admin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PropTradingSystem.Models
-{                                         
-    public class Admin
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<Report> Reports { get; set; }
-    }
-}
