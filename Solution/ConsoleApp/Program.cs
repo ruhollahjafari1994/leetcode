a375376fb05833ceccd183bc54ae2aa052ff20b8
Ruhollah JAfari <jruhollah@gmail.com>
2024-11-11 23:38:33 +0330

    1106. Parsing A Boolean Expression

--- Solution/ConsoleApp/Program.cs
+++ Solution/ConsoleApp/Program.cs
@@ -1,31 +1,68 @@
 ﻿using System;
+public class Solution
+{
+    public bool ParseBoolExpr(string expression)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in expression)
+        {
+            if (c == ')')
+            {  // End of an expression
+                List<char> operands = new List<char>();
+
+                // Pop until we get to the start of the expression
+                while (stack.Peek() != '(')
+                {
+                    operands.Add(stack.Pop());
+                }
+                stack.Pop();  // Remove the '('
+                char operatorChar = stack.Pop();  // Get the operator before '('
+
+                bool result;
+                if (operatorChar == '&')
+                {
+                    result = operands.All(op => op == 't');
+                }
+                else if (operatorChar == '|')
+                {
+                    result = operands.Any(op => op == 't');
+                }
+                else
+                { // operatorChar == '!'
+                    result = operands[0] == 'f';
+                }
+
+                // Push result back as 't' for true or 'f' for false
+                stack.Push(result ? 't' : 'f');
+            }
+            else if (c == 't' || c == 'f' || c == '!' || c == '&' || c == '|' || c == '(')
+            {
+                stack.Push(c);
+            }
+            // Ignore commas as they are separators
+        }
+
+        // The result will be the only item left in the stack
+        return stack.Pop() == 't';
+    }
+}
 
 class Program
 {
-    static int LinearSearch(int[] arr, int target)
-    {
-        for (int i = 0; i < arr.Length; i++)
-        {
-            // Check if the current element matches the target
-            if (arr[i] == target)
-            {
-                return i; // Return the index if found
-            }
-        }
-
-        return -1; // Return -1 if target is not found in the array
-    }
-
     static void Main()
     {
-        int[] arr = { 7, 2, 9, 4, 3, 8, 5 };
-        int target = 4;
+        Solution solution = new Solution();
 
-        int result = LinearSearch(arr, target);
+        // Test cases
+        string expression1 = "&(|(f))";
+        string expression2 = "|(f,f,f,t)";
+        string expression3 = "!(&(f,t))";
 
-        if (result == -1)
-            Console.WriteLine("Element not present in array");
-        else
-            Console.WriteLine("Element found at index " + result);
+        // Printing the results
+        Console.WriteLine("Expression: " + expression1 + " => " + solution.ParseBoolExpr(expression1)); // Expected: false
+        Console.WriteLine("Expression: " + expression2 + " => " + solution.ParseBoolExpr(expression2)); // Expected: true
+        Console.WriteLine("Expression: " + expression3 + " => " + solution.ParseBoolExpr(expression3)); // Expected: true
+
     }
 }
